1b37986bab1b9fd59bc3a436be1317a0097fe5d9
sanidhy-main <sanidhy.25bcon1241@jecrcu.edu.in>
2026-02-04 11:25:31 +0530

    Finished functions.js

---
+++ 01_basics/functions.js
@@ -0,0 +1,11 @@
+﻿function square(n) {
+    return n * n;
+}
+const isEven =  (n) => n % 2 === 0;
+
+console.log(square(8));
+console.log(isEven(6));
+console.log(isEven(7));
+
+console.log(5 == "5"); // '==' converts type (loose equality)
+console.log(5 === "5"); // '===' does not convert types (strict equality)
