6edb7fef2e227bcf69ddb289c896d602d9aa6cac
sanidhy-main <sanidhy.25bcon1241@jecrcu.edu.in>
2026-02-04 11:56:17 +0530

    Finished loops.js

---
+++ 01_basics/loops.js
@@ -0,0 +1,31 @@
+﻿//for loop
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+//for of loop
+const arr = ['a', 'b', 'c'];
+
+for (const i of arr) {
+    console.log(i);
+}
+
+//for in loop
+const obj = {a : 1, b : 2};
+
+for (const key in obj) {
+    console.log(key, obj[key]);
+}
+
+//break and continue
+for (let i = 1; i <= 10; i++) {
+    if (i === 8) {
+        break;
+    }
+
+    if (i % 2 !== 0) {
+        continue;
+    } else {
+        console.log(i);
+    }
+}
