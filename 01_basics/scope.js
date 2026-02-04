023ae0581863dc3218036b830f8826827ee74fdb
sanidhy-main <sanidhy.25bcon1241@jecrcu.edu.in>
2026-02-04 12:09:57 +0530

    Finished scope.js

---
+++ 01_basics/scope.js
@@ -0,0 +1,27 @@
+﻿//Global Scope
+let a = 1;
+
+//Function Scope
+function outer() {
+    let b = 2;
+
+    //Inside scope
+    if (true) {
+        let c = 3;
+        console.log(a, b, c); //Works
+    }
+
+    console.log(a, b); //Works
+    //console.log(c); does not work here (outside the 'if' statement)
+}
+
+console.log(a); //Works, as 'a' is a global variable
+
+outer(); //Works, as all variables are in scope inside the function when being logged
+
+// console.log(b); does not work here (outside the function)
+// console.log(c); does not work here (outside the function)
+
+//Note: The inner scope always overshadows any outer scope
+
+
