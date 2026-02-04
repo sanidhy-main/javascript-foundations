e6506fcfa8b838ed31145cd7086262d3fb0d15a2
sanidhy-main <sanidhy.25bcon1241@jecrcu.edu.in>
2026-02-04 11:39:05 +0530

    Finished conditionals.js

---
+++ 01_basics/conditionals.js
@@ -0,0 +1,20 @@
+﻿const number = 7;
+
+if (number % 2 === 0) {
+    console.log("Even");
+} else {
+    console.log("odd");
+}
+
+const temperature = 30;
+
+const weather = temperature  >= 25 ? "Hot" : "Cool";
+console.log(weather);
+
+//falsy statements
+    //false
+    //0
+    //"" or ''
+    //NaN
+    //Undefined
+    //null
