4fd2cb73628218dbe75a1c07b3feb3d08d82ac1d
sanidhy-main <sanidhy.25bcon1241@jecrcu.edu.in>
2026-02-10 16:52:24 +0530

    Finish callbacks.js

---
+++ 03_async/callbacks.js
@@ -0,0 +1,11 @@
+﻿function fetchData(callback) { //'callback' is a function
+    setTimeout(() => { //setTimeout(func, timeout)
+        console.log("Data Fetched");
+        callback();
+    },
+        10000); //sets timeout BEFORE executing 'func'
+}
+
+fetchData(() => {
+    console.log("Callback executed"); //callback function definition
+});
