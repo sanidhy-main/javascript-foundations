e56697bfc4e846b9e86ced162d56b6c86ce8ffc4
sanidhy-main <sanidhy.25bcon1241@jecrcu.edu.in>
2026-02-10 17:23:15 +0530

    Finished promises.js

---
+++ 03_async/promises.js
@@ -0,0 +1,10 @@
+﻿function fetchData() {
+    return new Promise((resolve, reject) => { //creates a 'promise'
+        setTimeout(() => {
+            resolve("Data fetched"); //if the promise is resolved.
+        }, 1000);
+    });
+}
+
+fetchData().then(data => console.log(data)) //when the promise succeeds
+fetchData().catch(err => console.log(err)) //when the promise fails
