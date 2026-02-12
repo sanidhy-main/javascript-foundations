f0c78ec17209d2be7f0241f729555e1054c1236a
sanidhy-main <sanidhy.25bcon1241@jecrcu.edu.in>
2026-02-12 17:07:03 +0530

    Finished async-await.js

---
+++ 03_async/async-await.js
@@ -0,0 +1,18 @@
+﻿function fetchData() {
+    return new Promise((resolve) => { //creates a promise
+        setTimeout(() => {
+            resolve("Data fetched"); //resolve output
+        }, 2000);
+    });
+}
+
+async function main() {  //async function
+    try {
+    const data = await fetchData(); //pauses until the promise is resolved
+    console.log(data);
+} catch (error) {
+    console.log(error);
+    }
+}
+
+main();
