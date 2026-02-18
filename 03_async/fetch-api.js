0667b70f3e7dbd2326676be60138b3c27a01b809
sanidhy-main <sanidhy.25bcon1241@jecrcu.edu.in>
2026-02-18 17:27:44 +0530

    Finished fetch-api.js

---
+++ 03_async/fetch-api.js
@@ -0,0 +1,19 @@
+﻿console.log("Script loaded");
+
+const butt = document.getElementById("loadBtn");
+const lis = document.getElementById("BtaskList");
+
+butt.addEventListener("click", async () => {
+    lis.innerHTML = "";
+
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos?_limit=5");
+    const data = await response.json();
+
+    data.forEach(todo => {
+        const li = document.createElement("li");
+        li.textContent = todo.title + (todo.completed ? " ✅" : " ❌");
+        list.appendChild(li);
+    });
+});
+
+
