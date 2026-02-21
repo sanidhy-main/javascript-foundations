28a28207d275e8c9cd9dc426f26c1312045f71b0
sanidhy-main <sanidhy.25bcon1241@jecrcu.edu.in>
2026-02-21 10:58:59 +0530

    Finished parse-stringify.js

---
+++ 04_data/parse-stringify.js
@@ -0,0 +1,11 @@
+﻿const user = {
+    name: "Sanidhy",
+    age: 18,
+    isStudent: true
+};
+
+const jsonString = JSON.stringify(user); //converts object to string (json)
+console.log(jsonString);
+
+const userObject = JSON.parse(jsonString); //converts json string to object
+console.log(userObject);
