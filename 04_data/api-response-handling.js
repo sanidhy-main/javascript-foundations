cfe2bb12fa310898e81bbd8e6672884d4175eba1
sanidhy-main <sanidhy.25bcon1241@jecrcu.edu.in>
2026-02-21 11:48:37 +0530

    Completed api-response-handling.js

---
+++ 04_data/api-response-handling.js
@@ -0,0 +1,17 @@
+﻿async function loadUsers() {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users");
+
+        if (!response.ok) {
+            throw new Error("Network response failed");
+        }
+
+        const data = await response.json();
+        console.log("Users: ", data);
+
+    } catch (error) {
+        console.log("Error: ", error.message);
+    }
+}
+
+loadUsers();
